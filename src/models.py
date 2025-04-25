43d8c2eb40a4b17f5c247c398a478d30409a6a55
Evgenia <evgeniabalackaa6@gmail.com>
2025-04-25 17:38:52 +0300

    report html

--- src/models.py
+++ src/models.py
@@ -9,7 +9,7 @@ class Product:
         return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт."
 
     def __add__(self, other):
-        if type(self) != type(other):
+        if not isinstance(other, self.__class__):
             raise TypeError("Нельзя складывать продукты разных типов.")
         return self.price * self.quantity + other.price * other.quantity
 
